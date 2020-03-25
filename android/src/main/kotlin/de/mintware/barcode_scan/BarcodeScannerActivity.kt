6258516f50fec813260a1d06f0f658e357925652
rajababu3 <rbtheroot@gmail.com>
2020-03-25 16:11:43 +0530

    few changes

--- android/src/main/kotlin/de/mintware/barcode_scan/BarcodeScannerActivity.kt
+++ android/src/main/kotlin/de/mintware/barcode_scan/BarcodeScannerActivity.kt
@@ -5,71 +5,48 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.zxing.BarcodeFormat
+import android.view.Menu
+import android.view.MenuItem
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+
 
 class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
 
     lateinit var scannerView: me.dm7.barcodescanner.zxing.ZXingScannerView
-    private lateinit var config: Protos.Configuration
 
     companion object {
-        const val REQUEST_TAKE_PHOTO_CAMERA_PERMISSION = 100
-        const val TOGGLE_FLASH = 200
-        const val EXTRA_CONFIG = "config"
-        const val CANCEL_BUTTON = 300
+        val REQUEST_TAKE_PHOTO_CAMERA_PERMISSION = 100
+        val TOGGLE_FLASH = 200
+        val CANCEL = 300
     }
 
-    // region Activity lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        config = Protos.Configuration.parseFrom(intent.extras!!.getByteArray(EXTRA_CONFIG))
-
         title = ""
         scannerView = ZXingScannerView(this)
         scannerView.setAutoFocus(true)
         // this paramter will make your HUAWEI phone works great!
         scannerView.setAspectTolerance(0.5f)
-
-        val restrictedFormats = mapRestrictedBarcodeTypes()
-        if (restrictedFormats.isNotEmpty()) {
-            scannerView.setFormats(restrictedFormats)
-        }
-
-        // this parameter will make your HUAWEI phone works great!
-        scannerView.setAspectTolerance(0.5f)
         setContentView(scannerView)
     }
 
-    override fun onPause() {
-        scannerView.stopCamera()
-        super.onPause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        scannerView.setResultHandler(this)
-
-        // start camera immediately if permission is already given
-        if (!requestCameraAccessIfNecessary()) {
-            startCamera()
-        }
-    }
-    // endregion
-
-    // region AppBar menu
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val buttonText = if (scannerView.flash) config.flashOffText else config.flashOnText
-        val item = menu.add(0, TOGGLE_FLASH, 0, buttonText)
+        if (scannerView.flash) {
+            val item = menu.add(0,
+                    TOGGLE_FLASH, 0, "Flash Off")
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        } else {
+            val item = menu.add(0,
+                    TOGGLE_FLASH, 0, "Flash On")
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        }
+
+        val item = menu.add(0, CANCEL, 0, "Cancel")
         item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-        val item2 = menu.add(1, CANCEL_BUTTON, 1, "Cancel")
-         item2.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -79,91 +56,88 @@ class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
             this.invalidateOptionsMenu()
             return true
         }
-         if (item.itemId == CANCEL_BUTTON) {
+        if (item.itemId == CANCEL) {
             finish()
             return true
         }
         return super.onOptionsItemSelected(item)
     }
-    // endregion
+
+    override fun onResume() {
+        super.onResume()
+        scannerView.setResultHandler(this)
+        // start camera immediately if permission is already given
+        if (!requestCameraAccessIfNecessary()) {
+            scannerView.startCamera()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scannerView.stopCamera()
+    }
 
     override fun handleResult(result: Result?) {
         val intent = Intent()
         intent.putExtra("SCAN_RESULT", result.toString())
-        setResult(RESULT_OK, intent)
+        setResult(Activity.RESULT_OK, intent)
         finish()
     }
 
-    private fun finishWithError(errorCode: String) {
+    fun finishWithError(errorCode: String) {
         val intent = Intent()
         intent.putExtra("ERROR_CODE", errorCode)
-        setResult(RESULT_CANCELED, intent)
+        setResult(Activity.RESULT_CANCELED, intent)
         finish()
     }
 
     private fun requestCameraAccessIfNecessary(): Boolean {
         val array = arrayOf(Manifest.permission.CAMERA)
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, array, REQUEST_TAKE_PHOTO_CAMERA_PERMISSION)
+        if (ContextCompat
+                .checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, array,
+                    REQUEST_TAKE_PHOTO_CAMERA_PERMISSION)
             return true
         }
         return false
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_TAKE_PHOTO_CAMERA_PERMISSION) {
-            if (verifyPermissions(grantResults)) {
-                startCamera()
-            } else {
-                finishWithError("PERMISSION_NOT_GRANTED")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_TAKE_PHOTO_CAMERA_PERMISSION -> {
+                if (PermissionUtil.verifyPermissions(grantResults)) {
+                    scannerView.startCamera()
+                } else {
+                    finishWithError("PERMISSION_NOT_GRANTED")
+                }
             }
-            return
+            else -> {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
         }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
+}
 
-    private fun startCamera() {
-        if (config.useCamera > -1) {
-            scannerView.startCamera(config.useCamera)
-        } else {
-            scannerView.startCamera()
-        }
-    }
+object PermissionUtil {
 
     /**
      * Check that all given permissions have been granted by verifying that each entry in the
      * given array is of the value [PackageManager.PERMISSION_GRANTED].
-
      * @see Activity.onRequestPermissionsResult
      */
-    private fun verifyPermissions(grantResults: IntArray): Boolean {
-        // Verify that each required permission has been granted, otherwise return false.
-        return grantResults.isNotEmpty() && grantResults.all {
-            it == PackageManager.PERMISSION_GRANTED
+    fun verifyPermissions(grantResults: IntArray): Boolean {
+        // At least one result must be checked.
+        if (grantResults.size < 1) {
+            return false
         }
-    }
 
-    private fun mapRestrictedBarcodeTypes(): List<BarcodeFormat> {
-        val types: MutableList<BarcodeFormat> = mutableListOf()
-
-        this.config.restrictFormatList.filterNotNull().forEach {
-            when (it) {
-                Protos.BarcodeFormat.aztec -> types.add(BarcodeFormat.AZTEC)
-                Protos.BarcodeFormat.code39 -> types.add(BarcodeFormat.CODE_39)
-                Protos.BarcodeFormat.code93 -> types.add(BarcodeFormat.CODE_93)
-                Protos.BarcodeFormat.code128 -> types.add(BarcodeFormat.CODE_128)
-                Protos.BarcodeFormat.dataMatrix -> types.add(BarcodeFormat.DATA_MATRIX)
-                Protos.BarcodeFormat.ean8 -> types.add(BarcodeFormat.EAN_8)
-                Protos.BarcodeFormat.ean13 -> types.add(BarcodeFormat.EAN_13)
-                Protos.BarcodeFormat.interleaved2of5 -> types.add(BarcodeFormat.ITF)
-                Protos.BarcodeFormat.pdf417 -> types.add(BarcodeFormat.PDF_417)
-                Protos.BarcodeFormat.qr -> types.add(BarcodeFormat.QR_CODE)
-                Protos.BarcodeFormat.upce -> types.add(BarcodeFormat.UPC_E)
-                Protos.BarcodeFormat.UNRECOGNIZED -> print("Unrecognized")
+        // Verify that each required permission has been granted, otherwise return false.
+        for (result in grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false
             }
         }
-
-        return types
+        return true
     }
 }
-
